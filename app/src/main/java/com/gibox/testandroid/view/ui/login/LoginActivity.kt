0fc82fc2a26a8bfb4937dbba747f5eb367c2407d
tffaj <taufik.fajari@akarinti.tech>
2022-07-13 18:54:40 +0700

    implement observe view model, edit text listener in logn activity

--- app/src/main/java/com/gibox/testandroid/view/ui/login/LoginActivity.kt
+++ app/src/main/java/com/gibox/testandroid/view/ui/login/LoginActivity.kt
@@ -9,14 +9,17 @@ package com.gibox.testandroid.view.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
 import com.gibox.testandroid.core.data.auth.source.remote.request.LoginRequest
 import com.gibox.testandroid.databinding.ActivityLoginBinding
 import com.gibox.testandroid.util.constant.FIELD_REQUIRED
 import com.gibox.testandroid.util.hideKeyboard
 import com.gibox.testandroid.util.openActivity
 import com.gibox.testandroid.util.showToast
+import com.gibox.testandroid.view.ui.dialog.LoadingDialog
 import com.gibox.testandroid.view.ui.viewmodel.MainViewModel
 import com.gibox.testandroid.view.ui.listuser.ListUserActivity
+import com.google.android.material.snackbar.Snackbar
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class LoginActivity : AppCompatActivity() {
@@ -29,5 +32,77 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        listenEditText()
+        initAction()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        mainViewModel.isLoadingRequestLogin.observe(this) {
+            if (it) {
+                showLoading()
+            } else {
+                dismissLoading()
+            }
+        }
+
+        mainViewModel.isErrorRequestLogin.observe(this){
+            if (!it.isNullOrEmpty()){
+                val snackBar = Snackbar.make(binding.root, it, Snackbar.LENGTH_LONG)
+                snackBar.show()
+            }
+        }
+
+        mainViewModel.dataRequestLogin.observe(this){
+            if (it.token?.isNullOrEmpty() == false && it.page != null){
+                val snackBar = Snackbar.make(binding.root, "success", Snackbar.LENGTH_LONG)
+                snackBar.show()
+            }
+        }
+    }
+
+    private var loadingDialog:LoadingDialog?=null
+    private fun showLoading(){
+        dismissLoading()
+        if(loadingDialog == null){
+            loadingDialog = LoadingDialog()
+            loadingDialog?.show(supportFragmentManager, LoadingDialog::class.java.simpleName)
+        }
+    }
+
+    private fun dismissLoading(){
+        if (loadingDialog != null){
+            loadingDialog?.dismiss()
+            loadingDialog = null
+        }
+    }
+
+    private fun listenEditText() {
+        binding.etEmail.doOnTextChanged { text, start, before, count ->
+            if (count <= 0){
+                binding.etlEmail.error = "Field is required"
+            }else{
+                binding.etlEmail.error = null
+            }
+        }
+
+        binding.etPassword.doOnTextChanged { text, start, before, count ->
+            if (count <= 0) {
+                binding.etlPassword.error = "Field is required"
+            } else {
+                binding.etPassword.error = null
+            }
+        }
+    }
+
+    private fun initAction() {
+        binding.btnLogin.setOnClickListener {
+            if(binding.etPassword.text?.isNotEmpty() == true && binding.etEmail.text?.isNotEmpty() == true){
+                mainViewModel.requestLogin(LoginRequest(
+                    email = binding.etEmail.text?.toString()?:"",
+                    password = binding.etPassword.text?.toString()?:""
+                ))
+            }
+        }
     }
 }
